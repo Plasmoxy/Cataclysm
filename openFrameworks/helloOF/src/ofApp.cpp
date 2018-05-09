db0cb994e735bfbf18af9b5ab76cb6eb610c4568
Plasmoxy <sebohacker@gmail.com>
2018-05-09 17:07:57 +0200

    uyyyyy

--- openFrameworks/helloOF/src/ofApp.cpp
+++ openFrameworks/helloOF/src/ofApp.cpp
@@ -1,71 +1,128 @@
 #include "ofApp.h"
+#include <iostream>
+
+
+// random background on start
+ofApp::ofApp() :
+	bgColor(30),
+	rectColor(getRandomColor()),
+	rectColorTimer(0)
+{
+
+}
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+
+	//ofEnableSmoothing();
+	//ofEnableAlphaBlending();
+	ofSetWindowTitle("helloOF - Plasmoxy");
+	ofSetFrameRate(60);
+	ofSetRectMode(OF_RECTMODE_CENTER);
+
+	ofBackground(bgColor);
+
+
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	updateMouse();
+
+	double dt = ofGetLastFrameTime(); // in milliseconds
+	updateRectColorTimer(dt);
 	
+	
+
+	ofPushStyle();
+	ofSetColor(rectColor);
+	ofDrawRectangle(mouse.x, mouse.y, 30, 30);
+	ofPopStyle();
+}
+
+
+// CUSTOM
+
+void ofApp::updateMouse() {
+	mouse.x = ofGetMouseX();
+	mouse.y = ofGetMouseY();
+}
+
+ofColor ofApp::getRandomColor() {
+	return ofColor(ofRandom(255), ofRandom(255), ofRandom(255));
+}
+
+void ofApp::updateRectColorTimer(double dt) {
+	rectColorTimer += dt;
+	
+	if (rectColorTimer > 1.0) { // after one second
+
+		rectColor = getRandomColor(); // change color
+
+		rectColorTimer = 0; // reset timer
+	}
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::draw(){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::mousePressed(int x, int y, int button) {
+	cout << "MOUSE PRESSED" << endl;
+
+	rectColor = getRandomColor();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
--- openFrameworks/helloOF/src/ofApp.h
+++ openFrameworks/helloOF/src/ofApp.h
@@ -2,23 +2,37 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
+public:
 
-	public:
-		void setup();
-		void update();
-		void draw();
+	ofColor bgColor, rectColor;
+	ofVec2f mouse;
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	double rectColorTimer;
+
+	ofApp();
+
+	void updateMouse();
+	void updateRectColorTimer(double dt);
+
+	// ------
+
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	ofColor getRandomColor();
+
 };
