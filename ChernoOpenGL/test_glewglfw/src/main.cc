7cdf937251c7ef864c490ca42a8c204818fb2747
Plasmoxy <sebohacker@gmail.com>
2020-05-06 19:24:14 +0200

    well wellw ell

---
+++ ChernoOpenGL/test_glewglfw/src/main.cc
@@ -0,0 +1,49 @@
+/*
+UPDATE: vcpkg didn't work, linked manually
+*/
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK)
+        std::cout << "GLEW init error." << std::endl;
+    
+
+    printf("GL version: %s\n", glGetString(GL_VERSION));
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
