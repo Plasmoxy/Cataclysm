3e06dcf6b8bd8da4981798689e3f21e9f6aabbf2
Plasmoxy <sebohacker@gmail.com>
2020-05-06 19:37:19 +0200

    rename

--- ChernoOpenGL/ChernoOpenGL_second/src/main.cc
+++ ChernoOpenGL/ChernoOpenGL_second/src/main.cc
@@ -37,6 +37,12 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
