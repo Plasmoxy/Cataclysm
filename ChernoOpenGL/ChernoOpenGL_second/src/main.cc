9d866f01bd19d4247e6170d769ee57ca36766ff6
Plasmoxy <sebohacker@gmail.com>
2020-05-06 22:09:52 +0200

    Update main.cc

--- ChernoOpenGL/ChernoOpenGL_second/src/main.cc
+++ ChernoOpenGL/ChernoOpenGL_second/src/main.cc
@@ -40,9 +40,21 @@ int main(void)
     GLuint buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-
-    // give him the data
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertices, GL_STATIC_DRAW);
+
+    // Vertex attributes = how data inside v. buffer is modelled.
+    // Typically the gpu provides max 16 4-component vertex attributes.
+    // This provides data layout for each vertex, while we have unknown amount
+    // of vertices.
+    // = 64 components per vertex
+    // eg. position atribute has = x and y = 2 components
+    // -> glVertexAttribPointer sets a SINGLE attribute of vertex
+    // index = index of attribute (max 15)
+    // size = count of components in the attribute
+    // stride = complete byte size of a vertex (all attributes)
+    // pointer = pointer inside the specific attribute (offset from vertex beginning)
+    // NOTE: to lay out data, use struct combined with offsetof macro
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), );
     
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
