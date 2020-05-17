d2af40411808c13bc59e706baeca8f787d5eca25
Plasmoxy <sebohacker@gmail.com>
2020-05-17 14:01:58 +0200

    templatess

--- Experiments/lib04_fmt/src/App.cc
+++ Experiments/lib04_fmt/src/App.cc
@@ -4,6 +4,6 @@
 #include <fmt/format.h>
 
 int main(int argc, char* argv[]) {
-	
+	fmt::print("xdxd");
 	return 0;
 }
--- _templates/C++17 Base Static/src/App.cc
+++ _templates/C++17 Base Static/src/App.cc
@@ -4,6 +4,6 @@
 #include <fmt/format.h>
 
 int main(int argc, char* argv[]) {
-	
+	fmt::print("Static XD\n");
 	return 0;
 }
